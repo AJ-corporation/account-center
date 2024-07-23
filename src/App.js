194eafe64388d1962cb15e438e2532d516473bbb
Akbar <akbarjonjorayev001@gmail.com>
2024-07-23 15:58:14 +0500

    Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './css/App.css'
+
+const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'))
 
 export default function App() {
   return (
     <>
-      <h1>Hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
