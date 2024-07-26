586d40883c246e7bad4c7924057e87a9918b33ac
Akbar <akbarjonjorayev001@gmail.com>
2024-07-26 23:57:26 +0500

    usage env instead of api.js

--- src/js/db/firebase/firebaseInit.js
+++ src/js/db/firebase/firebaseInit.js
@@ -1,6 +1,5 @@
-import { API } from '../../api/api'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-const app = initializeApp(API.firebaseConfig)
+const app = initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE))
 export const firebaseFirestore = getFirestore(app)
