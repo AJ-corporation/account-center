339bf4ba48790022192a6cd1b0e8f3e5dcbf0598
Akbar <akbarjonjorayev001@gmail.com>
2024-08-03 00:44:44 +0500

    usage of Checkbox

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 
 import './Checkbox.css'
 
-export default function Checkbox({ text }) {
+export default function Checkbox({ text, check }) {
   const [checked, setChecked] = useState(false)
+
+  function handleCheck() {
+    setChecked(!checked)
+    if (check) check(checked)
+  }
 
   return (
     <>
       <div
         className="checkbox_input_area list_x_small d_f_ce"
-        onClick={() => setChecked(!checked)}
+        onClick={handleCheck}
         isChecked={checked ? 'true' : ''}
       >
         <div className="checkbox_input"></div>
--- src/pages/SignupPage&LoginPage/SignupPage/components/SignupPagePassword.jsx
+++ src/pages/SignupPage&LoginPage/SignupPage/components/SignupPagePassword.jsx
@@ -1,12 +1,14 @@
 import { useContext, useEffect, useState } from 'react'
 
 import Input from '../../../../components/Input/Input'
+import Checkbox from '../../../../components/Checkbox/Checkbox'
 
 import { SignupPageContext } from '../SignupPageContext'
 import { isValidPasswords } from '../../../../js/utils/checker'
 
 export default function SignupPageNames() {
   const [error, setError] = useState('')
+  const [showPasswords, setShowPasswords] = useState(false)
   const { disabled, setDisabled, formData, setFormData, nextPage } =
     useContext(SignupPageContext)
 
@@ -29,7 +31,7 @@ export default function SignupPageNames() {
     <form className="input_area list_y d_f_jc_sb" onSubmit={nextPage}>
       <Input
         title="Password"
-        type="password"
+        type={showPasswords ? 'text' : 'password'}
         inputProps={{
           autoFocus: true,
           onChange: (e) =>
@@ -39,13 +41,19 @@ export default function SignupPageNames() {
       />
       <Input
         title="Confirm password"
-        type="password"
+        type={showPasswords ? 'text' : 'password'}
         inputProps={{
           onChange: (e) =>
             setFormData({ ...formData, confirmPassword: e.target.value }),
           value: formData.confirmPassword,
         }}
       />
+      <div className="d_f_jc_start">
+        <Checkbox
+          text="Show passwords"
+          check={(checked) => setShowPasswords(!checked)}
+        />
+      </div>
       {!error.ok && (
         <b className="con pd_tb_small txt_bg_red d_f_ce signup_login_error_txt">
           {error.error}
