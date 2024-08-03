1e106da099003b765c0079263dd086ce253b4dfb
Akbar <akbarjonjorayev001@gmail.com>
2024-08-04 00:23:24 +0500

    improve checking app

--- src/js/utils/checker.js
+++ src/js/utils/checker.js
@@ -2,21 +2,11 @@ import {
   loadFromLocalStorage,
   saveToLocalStorage,
 } from '../db/local/localStorage'
+import { getLocalData } from './checker.util'
 
 export function appChecker() {
   const localData = loadFromLocalStorage('aj-accounts')
   if (!localData) saveToLocalStorage('aj-accounts', getLocalData())
-}
-
-function getLocalData() {
-  const data = {
-    accounts: {
-      active: false,
-      accounts: [],
-    },
-  }
-
-  return data
 }
 
 export function isValidUsername(username) {
---
+++ src/js/utils/checker.util.js
@@ -0,0 +1,10 @@
+export function getLocalData() {
+  const data = {
+    accounts: {
+      active: false,
+      accounts: [],
+    },
+  }
+
+  return data
+}
