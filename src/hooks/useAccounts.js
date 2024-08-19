2aaae3f0e546dcee7bf3632e158fc8b62fa9178e
Akbar <akbarjonjorayev001@gmail.com>
2024-08-19 16:23:53 +0500

    significantly improved Firebase data retrieval speed

--- src/hooks/useAccounts.js
+++ src/hooks/useAccounts.js
@@ -2,12 +2,21 @@ import { useEffect, useState } from 'react'
 import { loadFromFirestore } from '../js/db/firebase/firebaseFirestore'
 import { downloadFromStore } from '../js/db/firebase/firebaseStorage'
 
+const accountsData = {
+  users: {},
+  profiles: {},
+}
+
 export function useGetAccount(id) {
   const [data, setData] = useState()
   useEffect(() => {
     async function loadData() {
-      const accountData = await loadFromFirestore('accounts', `${id}`)
-      setData(accountData)
+      if (accountsData.users.id) setData(accountsData.users.id)
+      if (!accountsData.users.id) {
+        const accountData = await loadFromFirestore('accounts', `${id}`)
+        accountsData.users.id = accountData
+        setData(accountData)
+      }
     }
     loadData()
   }, [id])
@@ -19,8 +28,12 @@ export function useGetAccountProfilePic(id) {
   const [data, setData] = useState()
   useEffect(() => {
     async function loadData() {
-      const accountData = await downloadFromStore(`users/${id}`)
-      setData(accountData)
+      if (accountsData.profiles.id) setData(accountsData.profiles.id)
+      if (!accountsData.profiles.id) {
+        const accountData = await downloadFromStore(`users/${id}/avatar`)
+        accountsData.profiles.id = accountData
+        setData(accountData)
+      }
     }
     loadData()
   }, [id])
