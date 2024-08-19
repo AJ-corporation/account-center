990c73d148869bcb2f29864bf809e7e93803daee
Akbar <akbarjonjorayev001@gmail.com>
2024-08-19 22:05:19 +0500

    dont show menu if user doesnt have account

--- src/components/Menu/MenuTop/MenuTop.jsx
+++ src/components/Menu/MenuTop/MenuTop.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react'
 
 import Avatar from '../../Avatar/Avatar'
 import Button from '../../Button/Button'
+import AccountListCreateOrLogin from '../../../pages/AccountsList/components/AccountListCreateOrLogin'
 
 import { loadFromLocalStorage } from '../../../js/db/local/localStorage'
 
@@ -16,17 +17,22 @@ export default function MenuTop() {
     <>
       <div className="pd_small d_f_ce">
         <div className="con_bg_df list_x d_f_ai_ce">
-          <Button className="d_f_ce pd_small">
-            <span className="material-symbols-outlined">search</span>
-          </Button>
-          <a href={`/profile/${curId}`}>
-            <Avatar
-              id="10"
-              className="menu_top_avatar cur_poi"
-              style={{ size: 36 }}
-              letter="a"
-            />
-          </a>
+          {curId && (
+            <>
+              <Button className="d_f_ce pd_small">
+                <span className="material-symbols-outlined">search</span>
+              </Button>
+              <a href={`/profile/${curId}`}>
+                <Avatar
+                  id="10"
+                  className="menu_top_avatar cur_poi"
+                  style={{ size: 36 }}
+                  letter="a"
+                />
+              </a>
+            </>
+          )}
+          {!curId && <AccountListCreateOrLogin />}
         </div>
       </div>
     </>
