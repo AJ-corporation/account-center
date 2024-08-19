05b9e186b90e126e57ad47101d839c2ff27e8e45
Akbar <akbarjonjorayev001@gmail.com>
2024-08-19 21:01:56 +0500

    Account List UI improved

--- src/pages/AccountsList/AccountsList.jsx
+++ src/pages/AccountsList/AccountsList.jsx
@@ -1,7 +1,9 @@
 import { useRef } from 'react'
 
 import { AccountData } from './components/AccountData'
+import Button from '../../components/Button/Button'
 
+import { goToHref } from '../../js/utils/href'
 import { loadFromLocalStorage } from '../../js/db/local/localStorage'
 
 import './AccountsList.css'
@@ -11,13 +13,25 @@ export default function AccountsList() {
 
   return (
     <>
-      <div className="full_page d_f_ce list_y_small">
-        <AccountData className="accounts_list_active" id={localData?.active} />
-        <hr className="w_100" />
-        {localData?.accounts &&
-          localData?.accounts.map(
-            (id) => id !== localData?.active && <AccountData key={id} id={id} />
-          )}
+      <div className="full_page d_f_ce">
+        <div className="account_list_con list_y_small">
+          <div className="list_y_small">
+            <AccountData
+              className="accounts_list_active"
+              id={localData?.active}
+              logout={true}
+            />
+            <Button className="clr_bd_btn w_100" onClick={() => goToHref('/')}>
+              Main page
+            </Button>
+          </div>
+          <hr className="w_100" />
+          {localData?.accounts &&
+            localData?.accounts.map(
+              (id) =>
+                id !== localData?.active && <AccountData key={id} id={id} />
+            )}
+        </div>
       </div>
     </>
   )
--- src/pages/AccountsList/components/AccountData.jsx
+++ src/pages/AccountsList/components/AccountData.jsx
@@ -10,33 +10,41 @@ export function AccountData({ className, id, logout = false }) {
 
   return (
     <>
-      <div className={`con_bg_df account_data ${className}`}>
-        <div className="list_x d_f_jc_sb">
-          <div className="list_x">
-            <Avatar
-              style={{ size: 40 }}
-              id={id}
-              letter={accountData?.user.fname[0]}
-            />
-            <div className="list_y_small_very d_f_jc_ce">
-              <div>
-                {`${accountData?.user.fname || 'Loading'} ${
-                  accountData?.user?.lname || ''
-                }`}
-              </div>
-              <div className="account_data_username">
-                {accountData?.user.username
-                  ? `@${accountData?.user.username}`
-                  : ''}
-              </div>
-              <div></div>
-            </div>
+      {logout && (
+        <div className={`con_bg_dr d_f_jc_sb ${className}`}>
+          <AccountDataItem id={id} accountData={accountData} />
+          <Button className="d_f_ce txt_red pd_small">
+            <span className="material-symbols-outlined">logout</span>
+          </Button>
+        </div>
+      )}
+      {!logout && (
+        <Button className="list_x">
+          <AccountDataItem id={id} accountData={accountData} />
+        </Button>
+      )}
+    </>
+  )
+}
+
+function AccountDataItem({ id, accountData }) {
+  return (
+    <>
+      <div className="list_x_small">
+        <Avatar
+          style={{ size: 35 }}
+          id={id}
+          letter={accountData?.user.fname[0]}
+        />
+        <div className="list_y_small_very d_f_ai_start">
+          <div>
+            {`${accountData?.user.fname || 'Loading'} ${
+              accountData?.user?.lname || ''
+            }`}
           </div>
-          {logout && (
-            <Button className="d_f_ce txt_red pd_small">
-              <span className="material-symbols-outlined">logout</span>
-            </Button>
-          )}
+          <div className="account_data_username">
+            {accountData?.user.username ? `@${accountData?.user.username}` : ''}
+          </div>
         </div>
       </div>
     </>
